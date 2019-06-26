2e7d6ac9eb1a8e122cca0477db5fc90c204573b2
pmacrory <pmacro@icloud.com>
2019-06-26 09:21:27 -0700

    Improve error handling

--- Sources/HighlighterRunner/main.swift
+++ Sources/HighlighterRunner/main.swift
@@ -9,11 +9,12 @@ if let filePath = CommandLine.arguments.last,
   FileManager.default.fileExists(atPath: filePath) {
   let fileURL = URL(fileURLWithPath: filePath)
   
-  let tokens = try? Highlighter().highlight(fileURL)
-  if let tokens = tokens {
-    if let tokenData = try? JSONEncoder().encode(tokens) {
-      print(String(data: tokenData, encoding: .utf8) ?? errorJson)
-    }
+  do {
+    let tokens = try Highlighter().highlight(fileURL)
+    let tokenData = try JSONEncoder().encode(tokens)
+    print(String(data: tokenData, encoding: .utf8) ?? errorJson)
+  } catch let error {
+    print("[{\"error\" : \"\(error)\"}]")  
   }
 } else {
   print(errorJson)
